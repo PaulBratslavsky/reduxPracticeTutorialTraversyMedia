3cb25360519fb02d4cbe30fcf2e2361ea6d4115c
Pablo Diablo <pablodiablo@Pablos-MacBook-Pro.local>
2018-04-09 15:09:59 -0400

    ready for redux

--- src/components/Postform.js
+++ src/components/Postform.js
@@ -10,20 +10,40 @@ class PostForms extends Component {
         };
 
         this.onChange = this.onChange.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
 
     }
 
     onChange(event) {
-        event.preventDefault(); 
         console.log('from on change event');
         this.setState({ [event.target.name]: event.target.value });
+    }
+
+    onSubmit(event) {
+        event.preventDefault(); 
+
+        const post = {
+            title: this.state.title,
+            body: this.state.body,
+        };
+
+        fetch('https://jsonplaceholder.typicode.com/posts', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(post)
+        }) 
+        .then( response => response.json() )
+        .then( data => console.log(data) )
+
     }
 
   render() {
     return (
       <div>
         <h2>Hello from Form</h2>
-        <form> 
+        <form onSubmit={this.onSubmit}> 
             <div>
                 <label>Title</label><br />
                 <input type="text" name="title" onChange={this.onChange} value={this.state.title}/>    
